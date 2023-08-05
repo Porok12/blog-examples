4de09a17f5b3120b5492ebf18ad3aecde963a164
przemek <przemekpapla@gmail.com>
2023-08-05 21:48:01 +0200

    update main.c

--- raspberry-pico-c-blinking-led/main.c
+++ raspberry-pico-c-blinking-led/main.c
@@ -1,5 +1,9 @@
 #include "pico/stdlib.h"
 
+void gpio_toggle(uint pin) {
+    gpio_put(pin, !gpio_get(pin));
+}
+
 int main() {
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
@@ -8,9 +12,7 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
+        gpio_toggle(LED_PIN);
         sleep_ms(250);
     }
 #endif
